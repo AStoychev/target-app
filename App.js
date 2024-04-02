065d838cf1a492c5afaf1480485e054d3eb8df2d
Stoychev <stoychev.nas@gmail.com>
2024-04-02 14:54:10 +0300

    Complete target app from tutorial - can create changes

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { StyleSheet, View, ImageBackground, SafeAreaView } from 'react-native';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
@@ -8,14 +10,30 @@ import GameOverScreen from './screens/GameOverScreen';
 export default function App() {
     const [userNumber, setUserNumber] = useState();
     const [gameIsOver, setGameIsOver] = useState(true);
+    const [guessRounds, setGuessRounds] = useState(0);
+
+    const [fontsLoaded] = useFonts({
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />
+    }
 
     function pickedNumberHandler(pickedNumber) {
         setUserNumber(pickedNumber);
         setGameIsOver(false);
     }
 
-    function gameOverHandler() {
+    function gameOverHandler(numberOfRounds) {
         setGameIsOver(true);
+        setGuessRounds(numberOfRounds);
+    }
+
+    function startNewGameHandler() {
+        setUserNumber(null);
+        setGuessRounds(0);
     }
 
     let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />
@@ -25,7 +43,7 @@ export default function App() {
     }
 
     if (gameIsOver && userNumber) {
-        screen = <GameOverScreen />
+        screen = <GameOverScreen userNumber={userNumber} roundsNumber={guessRounds} onStartNewGame={startNewGameHandler}/>
     }
 
     return (
